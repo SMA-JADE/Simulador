9b1788107aa2f4c2bd3f616343db33539ac7f413
Nan Montaño <nano.mv112@gmail.com>
2016-04-22 12:58:40 -0700

    init

---
+++ main/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    /**
+     * Aqui corremos jade.Boot con nuestros agentitos. En vez de mandar argumentos
+     * por consola, los mandamos aqui y asi (R)
+     * @param args console args
+     */
+    public static void main(String[] args) {
+        jade.Boot.main(new String[]{
+                "-gui",
+                //Agentito:ClaseAgente(args);OtroAgente....
+        });
+    }
+}
