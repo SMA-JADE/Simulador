c5e5638d647fa6f008ce2ba223f80bca0a51668e
Gerardo <geratarra@gmail.com>
2016-05-17 06:22:21 -0600

    Cliente espera por la pizza!

--- main/src/agents/ClientAgent.java
+++ main/src/agents/ClientAgent.java
@@ -6,14 +6,14 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 
 /**
- * Created by gerardo on 27/04/16.
+ * @author Gerardo
  */
 public class ClientAgent extends Agent {
 
     protected void setup() {
         ACLMessage order = new ACLMessage();
         order.addReceiver(new AID("PromotorAgent", false));
-        order.setContent("Quiero una hamburgesa.");
+        order.setContent("Quiero una pizza bien Hot & Ready.");
         send(order);
         System.out.print("Orden del cliente " + getName() + " pedida.\n");
         addBehaviour(new CyclicBehaviour(this) {
@@ -21,8 +21,14 @@ public class ClientAgent extends Agent {
                 ACLMessage msg = blockingReceive();
                 System.out.print("Promotor envio mensaje..\n");
                 if (msg != null) {
-                    System.out.println(msg.getContent());
-                    myAgent.doDelete();
+                    if (msg.getContent().equals("Favor de esperar un momento..")) {
+                        System.out.println("Agente >> "+myAgent.getLocalName()+", en espera de una pizzona");
+                        myAgent.doSuspend(); // Para reactivar el agente usar: doActivate()
+                        System.out.println("Agente >> "+myAgent.getLocalName()+", suspendido");
+                    } else {
+                        System.out.println(msg.getContent());
+                        myAgent.doDelete();
+                    }
                 }
             }
         });
