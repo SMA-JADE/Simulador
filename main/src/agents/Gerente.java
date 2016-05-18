9982b4a67d7c9e7d6d3c501c64b69a73a7491d17
Gerardo <geratarra@gmail.com>
2016-05-18 07:02:53 -0600

    Gerente checa cuantas pizzas hay

--- main/src/agents/Gerente.java
+++ main/src/agents/Gerente.java
@@ -1,6 +1,11 @@
 package agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+import util.ResourcesManager;
 
 /**
  * @author Gerardo
@@ -9,7 +14,15 @@ import jade.core.Agent;
 public class Gerente extends Agent {
 
     protected void setup() {
-
+        addBehaviour(new TickerBehaviour(this, 2501) {
+            public void onTick() {
+                // Por lo pronto, el limite sera 20 :)
+                if (ResourcesManager.getPizzasLength() <= 20) {
+                    ACLMessage mas_pizzas = new ACLMessage();
+                    mas_pizzas.setContent("Cantidad de pizzas menor a 20, favor de ponerse a trabajar");
+                    send(mas_pizzas);
+                }
+            }
     }
 
 }
