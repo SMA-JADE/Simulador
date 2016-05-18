86f36bf80b5c5c1272dcc6cb19b72773e3dd39ea
Gerardo <geratarra@gmail.com>
2016-05-18 06:48:33 -0600

    Anadi clase gerente y metodo para obtener num. de pizzas en RM

---
+++ main/src/agents/Gerente.java
@@ -0,0 +1,15 @@
+package agents;
+
+import jade.core.Agent;
+
+/**
+ * @author Gerardo
+ */
+
+public class Gerente extends Agent {
+
+    protected void setup() {
+
+    }
+
+}
--- main/src/util/ResourcesManager.java
+++ main/src/util/ResourcesManager.java
@@ -71,6 +71,8 @@ public class ResourcesManager {
         return ordenes.size() == 0;
     }
 
+    public static int getPizzasLength() { return pizzas.size(); }
+
 
 
 
