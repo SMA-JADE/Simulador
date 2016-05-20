0e4cd60996e41c3594ae3ac2de4a1e9f8d4dfed5
Gerardo <geratarra@gmail.com>
2016-05-19 18:29:55 -0600

    Gerente crea orden

--- main/src/agents/Gerente.java
+++ main/src/agents/Gerente.java
@@ -5,6 +5,7 @@ import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.wrapper.StaleProxyException;
 import util.ResourcesManager;
 
 /**
@@ -18,9 +19,11 @@ public class Gerente extends Agent {
             public void onTick() {
                 // Por lo pronto, el limite sera 20 :)
                 if (ResourcesManager.getPizzasLength() <= 20) {
-                    ACLMessage mas_pizzas = new ACLMessage();
-                    mas_pizzas.setContent("Cantidad de pizzas menor a 20, favor de ponerse a trabajar");
-                    send(mas_pizzas);
+                    try {
+                        getContainerController().createNewAgent("Gerente", "agents.OrderAgent", null);
+                    } catch (StaleProxyException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
